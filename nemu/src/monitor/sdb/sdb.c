8004bc977d7df49f81a3f3c24c6e078156fb8a77
tracer-ics2021 <tracer@njuics.org>
2021-10-01 00:19:34 +0800

    >  compile 201220000    Static hostname: yu          Icon name: computer-vm            Chassis: vm         Machine ID: 7f502ffebf874aa0abace6c8294260fb            Boot ID: 1302385bc7ae436289792c7ea40ef8f5     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  00:19:34 up 45 min,  1 user,  load average: 0.11, 0.28, 0.34

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -42,8 +42,8 @@ static int cmd_si(char *args) {
         // 判断是否有多个参数
         char *arg_end = arg + strlen(arg) - 1;
         char *args_end = args + strlen(args) - 1;
-        printf("%c\n", *arg_end);
-        printf("%c\n", *args_end);
+        printf("%c\n", *arg);
+        printf("%c\n", *args);
         if (arg_end < args_end) {
             printf("too many args: %c\n", *args);
             return 0;
